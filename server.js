f6b0a77d4ed4ff498984121ae01532007b03b973
lafrus <firasmat57@gmail.com>
2023-12-01 19:47:44 +0100

    Your informative commit message here

---
+++ server.js
@@ -0,0 +1,37 @@
+// server.js
+
+const express = require('express');
+const mongoose = require('mongoose');
+const socketIO = require('socket.io');
+const http = require('http');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIO(server);
+
+const PORT = process.env.PORT || 3000;
+
+// Middleware
+app.use(express.json());
+
+// MongoDB connection
+const mongoURI = 'mongodb+srv://firasmat:Chadlia@gmail.com23@cluster0.wal9zk8.mongodb.net/';
+mongoose.connect(mongoURI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+// Event listener for successful MongoDB connection
+mongoose.connection.on('connected', () => {
+  console.log('Connected to MongoDB');
+});
+
+// Event listener for MongoDB connection error
+mongoose.connection.on('error', (err) => {
+  console.error('MongoDB connection error:', err);
+});
+
+// Start the server
+server.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});

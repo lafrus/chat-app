e10206c1b63c2815a8a6211ea729e736b1f2c8a1
lafrus <firasmat57@gmail.com>
2023-12-02 15:04:07 +0100

    Your commit message

---
+++ ChatRoom.js
@@ -0,0 +1,13 @@
+// ChatRoom.js
+import React from 'react';
+import './ChatRoom.css'; // This is where your styling will be imported
+
+const ChatRoom = () => {
+  return (
+    <div className="chatRoom">
+      {/* Chat room content goes here */}
+    </div>
+  );
+};
+
+export default ChatRoom;
